976f98725917f496817d113b938c339ee7b88bf6
janluisho <mixedase@gmail.com>
2023-11-25 14:02:18 +0100

    add db

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_from_directory, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.sqlite"
+
+
+db = SQLAlchemy()
+db.init_app(app)
 
 
 @app.route('/')
