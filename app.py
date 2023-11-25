8dc2acde90c785ce3cd7b87655b939c10f016e1a
janluisho <mixedase@gmail.com>
2023-11-25 15:17:30 +0100

    create first simple homepage

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_from_directory, redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.sqlite"
@@ -10,8 +11,13 @@ db.init_app(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        ip_addr = request.environ['REMOTE_ADDR']
+    else:
+        ip_addr = request.environ['HTTP_X_FORWARDED_FOR']  # if behind a prox
+
+    return render_template("index.html", ip_addr=ip_addr)
 
 
 if __name__ == '__main__':
