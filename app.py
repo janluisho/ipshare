a2f353b8db99331b1e66851e84c3a3c02871db38
janluisho <mixedase@gmail.com>
2023-11-25 16:06:09 +0100

    add impressum.html and "now", "register", "signin" routes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request, send_from_directory, redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.sqlite"
@@ -11,13 +10,37 @@ db.init_app(app)
 
 
 @app.route('/')
-def hello_world():
+def root():
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         ip_addr = request.environ['REMOTE_ADDR']
     else:
         ip_addr = request.environ['HTTP_X_FORWARDED_FOR']  # if behind a prox
 
     return render_template("index.html", ip_addr=ip_addr)
+
+
+@app.route('/now')
+def now():
+    """Teilt Ip sofort"""
+    return "todo"
+
+
+@app.route('/register')
+def register():
+    """Account Erstellen"""
+    return "todo"
+
+
+@app.route('/signin')
+def signin():
+    """Login"""
+    return "todo"
+
+
+@app.route('/impressum')
+def impressum():
+    """Impressum"""
+    return render_template("impressum.html")
 
 
 if __name__ == '__main__':
