053a47396b6e94b2a49de0c2662d61ea7259c560
janluisho <mixedase@gmail.com>
2023-11-25 18:11:49 +0100

    db: User, SharedAddresses

---
+++ db.py
@@ -0,0 +1,22 @@
+from app import app, db
+from sqlalchemy.sql import func
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id: db.Mapped[int] = db.mapped_column(db.Integer, primary_key=True)
+    name: db.Mapped[str] = db.mapped_column(db.String(69), unique=True)
+    password: db.Mapped[str] = db.mapped_column(db.String(69))
+    # salt also stored in password
+
+
+class SharedAddresses(db.Model):
+    user: db.Mapped[int] = db.mapped_column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    device_name: db.Mapped[str] = db.mapped_column(db.String(420), primary_key=True)
+    address: db.Mapped[str] = db.mapped_column(db.String(420))
+    last_updated: db.Mapped[db.DateTime] = db.mapped_column(db.DateTime, onupdate=func.now())
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
