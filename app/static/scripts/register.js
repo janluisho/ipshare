7e05c215367842ddab0fc084acecccd05077e9c8
janluisho <mixedase@gmail.com>
2023-12-05 00:09:18 +0100

    desc_heading

--- app/static/scripts/register.js
+++ app/static/scripts/register.js
@@ -1,5 +1,7 @@
+const desc_heading = document.getElementById("desc_heading");
 const desc_text = document.getElementById("desc_text");
 desc_text.dataset.desc = desc_text.innerText
+desc_heading.dataset.desc = desc_heading.innerText
 
 const typewriter_intervall_handler = (target, element, data) => {
     return () => {
@@ -22,14 +24,27 @@ document.querySelectorAll('#name, #password').forEach(function (element) {
     element.inverval = null;
 
     element.onfocus = event => {
+        desc_heading.iteration = 0;
+        clearInterval(desc_heading.inverval);
+        desc_heading.inverval = setInterval(typewriter_intervall_handler(
+            desc_heading, desc_heading, event.target.placeholder
+        ), 20);
+
         desc_text.iteration = 0;
         clearInterval(desc_text.inverval);
-        desc_text.inverval = setInterval(typewriter_intervall_handler(
-            desc_text, desc_text, event.target.dataset.desc
-        ), 20);
+            desc_text.inverval = setInterval(typewriter_intervall_handler(
+                desc_text, desc_text, event.target.dataset.desc
+            ), 20);
     }
     element.onblur = event => {
+        desc_heading.iteration = 0;
+        clearInterval(desc_heading.inverval);
+        desc_heading.inverval = setInterval(typewriter_intervall_handler(
+            desc_heading, desc_heading, desc_heading.dataset.desc
+        ), 20);
+
         desc_text.iteration = 0;
+        desc_text.innerText = "";
         clearInterval(desc_text.inverval);
         desc_text.inverval = setInterval(typewriter_intervall_handler(
             desc_text, desc_text, desc_text.dataset.desc
