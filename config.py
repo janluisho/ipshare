8a2ac8d210e617230be830bc4f958850bc9a5d78
janluisho <mixedase@gmail.com>
2024-01-19 08:04:30 +0100

    config.py as template

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import datetime
+
 SQLALCHEMY_DATABASE_URI = "sqlite:///database.sqlite"
 SECRET_KEY = "secret_key"  # open("secret_key").readline()  # todo
-
+CSRF_SECRET_KEY = "csrf_secret_key"  # todo
+JWT_SECRET_KEY = "jwt_secret_key"  # todo
+# PERMANENT_SESSION_LIFETIME = 6000
+# REMEMBER_COOKIE_DURATION = datetime.timedelta(days=42)
+# REMEMBER_COOKIE_DOMAIN =   # todo
+# REMEMBER_COOKIE_SAMESITE = True
