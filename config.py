dd9e14deee0e67aa228ab1f31970183dbeefba3c
janluisho <mixedase@gmail.com>
2024-02-02 18:10:56 +0100

    VISITOR_PASSWORD

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///database.sqlite"
 SECRET_KEY = "secret_key"  # open("secret_key").readline()  # todo
 CSRF_SECRET_KEY = "csrf_secret_key"  # todo
 JWT_SECRET_KEY = "jwt_secret_key"  # todo
+VISITOR_PASSWORD = "visitor_password"  # todo
 # PERMANENT_SESSION_LIFETIME = 6000
 # REMEMBER_COOKIE_DURATION = datetime.timedelta(days=42)
 # REMEMBER_COOKIE_DOMAIN =   # todo
